692c47913076b86cff3914566c0084b4133c606d
jones1008 <jones@fluhrers.de>
2020-03-06 18:17:06 +0100

    made login button functional

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,12 +1,21 @@
 <template>
-    <div>
-        <v-btn small>Anmelden</v-btn>
+    <div style="text-align: center">
+        <h1>MyEvents</h1>
+        <p>Find personalized music events with Spotify</p>
+        <v-btn link :href="loginLink">Login to Spotify</v-btn>
     </div>
 </template>
 
 <script>
+    import config from "../config.js";
+    import router from "../router";
     export default {
-        name: "Home"
+        name: "Home",
+        computed: {
+            loginLink: function () {
+                return config.apiUrl + "authenticate";
+            }
+        },
     }
 </script>
 
