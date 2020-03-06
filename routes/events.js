45eec58cb7701bc8e7551843e054b000cbf3c34b
David Hein <david-hein196@web.de>
2020-03-06 09:12:04 +0100

    Add file for route events

---
+++ routes/events.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+var applicationId = 'rd6RC8JknxGKq89Q';
+
+router.post('/', function(req, res, next) {
+
+});
+
+module.exports = router;
