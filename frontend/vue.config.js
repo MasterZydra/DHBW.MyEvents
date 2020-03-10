f378d637e4339e26ce8f6daa931d250230f598fe
jones1008 <jones@fluhrers.de>
2020-03-10 11:41:18 +0100

    restructured for future easier docker implementation

---
+++ frontend/vue.config.js
@@ -0,0 +1,15 @@
+// const frontendPath = "frontend";
+//
+// module.exports = {
+//   "publicPath": "/"+frontendPath,
+//   "transpileDependencies": [
+//     "vuetify"
+//   ],
+//   chainWebpack: config => {
+//     config
+//         .entry("app")
+//         .clear()
+//         .add("./"+frontendPath+"/src/main.js")
+//         .end();
+//   }
+// };
