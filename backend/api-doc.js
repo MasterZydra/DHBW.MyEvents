a0e03341387598875670c7c06d764f9ce34b2236
David Hein <david-hein196@web.de>
2020-03-11 14:14:43 +0100

    Add all routes

--- backend/api-doc.js
+++ backend/api-doc.js
@@ -7,7 +7,132 @@ const apiDoc = {
     },
     definitions: {
     },
-    paths: {}
+    paths: {
+        "/authenticate": {
+            parameters:[],
+            post: {
+                summary: "Returns worlds by name.",
+                operationId:"postWorlds",
+                parameters: [
+                    {
+                        in: "query",
+                        name:"worldName",
+                        required:true,
+                        type:"string"
+                    }],
+                responses: {
+                    200: {
+                        description:"A list of worlds that match the requested name.",
+                        schema:{
+                            type:"array",
+                            items:{
+                                "$ref":"#/definitions/World"
+                            }
+                        }
+                        },
+                    default: {
+                        description: "An error occurred",
+                        schema: {
+                            additionalProperties:true
+                        }
+                    }
+                }
+            }
+        },
+        "/events": {
+            parameters:[],
+            post: {
+                summary: "Returns worlds by name.",
+                operationId:"postWorlds",
+                parameters: [
+                    {
+                        in: "query",
+                        name:"worldName",
+                        required:true,
+                        type:"string"
+                    }],
+                responses: {
+                    200: {
+                        description:"A list of worlds that match the requested name.",
+                        schema:{
+                            type:"array",
+                            items:{
+                                "$ref":"#/definitions/World"
+                            }
+                        }
+                    },
+                    default: {
+                        description: "An error occurred",
+                        schema: {
+                            additionalProperties:true
+                        }
+                    }
+                }
+            }
+        },
+        "/getGenres": {
+            parameters:[],
+            post: {
+                summary: "Returns worlds by name.",
+                operationId:"postWorlds",
+                parameters: [
+                    {
+                        in: "query",
+                        name:"worldName",
+                        required:true,
+                        type:"string"
+                    }],
+                responses: {
+                    200: {
+                        description:"A list of worlds that match the requested name.",
+                        schema:{
+                            type:"array",
+                            items:{
+                                "$ref":"#/definitions/World"
+                            }
+                        }
+                    },
+                    default: {
+                        description: "An error occurred",
+                        schema: {
+                            additionalProperties:true
+                        }
+                    }
+                }
+            }
+        },
+        "/getSpotifyAccessToken": {
+            parameters:[],
+            post: {
+                summary: "Returns worlds by name.",
+                operationId:"postWorlds",
+                parameters: [
+                    {
+                        in: "query",
+                        name:"worldName",
+                        required:true,
+                        type:"string"
+                    }],
+                responses: {
+                    200: {
+                        description:"A list of worlds that match the requested name.",
+                        schema:{
+                            type:"array",
+                            items:{
+                                "$ref":"#/definitions/World"
+                            }
+                        }
+                    },
+                    default: {
+                        description: "An error occurred",
+                        schema: {
+                            additionalProperties:true
+                        }
+                    }
+                }
+            }
+        },
+    }
 };
 
 module.exports = apiDoc;
