2d7cd1d8d83373d86442600ab2575b37c483d79c
David Hein <david-hein196@web.de>
2020-03-11 11:30:27 +0100

    Add api-doc for API in general

---
+++ backend/api-doc.js
@@ -0,0 +1,13 @@
+const apiDoc = {
+    swagger: '2.0',
+    basePath: '/',
+    info: {
+        title: 'MyEvents - Events matching your music taste',
+        version: '1.0.0'
+    },
+    definitions: {
+    },
+    paths: {}
+};
+
+module.exports = apiDoc;
