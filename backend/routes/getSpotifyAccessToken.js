849b7f1714b10b221c5067068798c997a653f20b
trigeha <51359838+trigeha@users.noreply.github.com>
2020-03-11 16:04:38 +0100

    Add refresh access_token

--- backend/routes/getSpotifyAccessToken.js
+++ backend/routes/getSpotifyAccessToken.js
@@ -17,6 +17,29 @@ async function getSpotifyAccessToken(code, redirect_uri) {
     };
 
     return await axios(authOptions).then(function (response) {
+        return Promise.resolve({
+            access_token: response.data.access_token,
+            refresh_token: response.data.refresh_token
+        });
+    }).catch(function (error) {
+        return Promise.reject(error);
+    });
+}
+
+async function refreshToken(refresh_token) {
+    let data = 'refresh_token=' + refresh_token + '&grant_type=refresh_token';
+    let authOptions = {
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        data: data,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + (new Buffer(config.spotify.client_id + ':' + config.spotify.client_secret).toString('base64'))
+        },
+        json: true
+    };
+
+    return await axios(authOptions).then(function (response) {
         return Promise.resolve(response.data.access_token);
     }).catch(function (error) {
         return Promise.reject(error);
@@ -30,6 +53,15 @@ router.post('/', function(req, res, next) {
         console.log(error);
         res.status(error.response.status).send(error.response.statusText);
     });
+});
+
+router.post('/refresh', function (req, res, next) {
+    refreshToken(req.body.refresh_token).then(function (access_token) {
+        res.send(access_token);
+    }).catch(function (error) {
+        console.log(error);
+        res.status(error.response.status).send(error.response.statusText);
+    })
 });
 
 module.exports = {
